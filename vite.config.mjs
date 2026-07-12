6418ef9ae0a48d131fb1dd12416a802b095028a4
GanDaicheng <GanDaicheng@users.noreply.github.com>
2026-07-12 14:25:50 +0800

    Support Vercel root deployment

--- vite.config.mjs
+++ vite.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/ganden-ai-portfolio/",
+  base: process.env.VERCEL ? "/" : "/ganden-ai-portfolio/",
   plugins: [react()],
 });
